3ef1724f6f0ee791849fc17d46d4cca9fd5a1283
Noe Nieto <noe.nieto@noenieto.com>
2020-06-08 02:10:55 -0700

    First working version

--- src/App.js
+++ src/App.js
@@ -3,131 +3,186 @@ import './tailwind.generated.css';
 import circuit from './LM317.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <h1 class="text-gray-900 text-3xl title-font font-medium mb-4">LM317 simulator</h1>
-      </header>
-      <section class="text-gray-700 body-font relative">
-        <div class="container px-5 py-24 mx-auto flex sm:flex-no-wrap flex-wrap">
-          <div class="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-            <object type="image/svg+xml" data={circuit}>
-                <img src={circuit} />
-            </object>
-          </div>
-          <div class="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-            {/* V_in */}
-            <div class="md:flex md:items-center mb-6">
-                <div class="md:w-1/4">
-                  <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="V_in">
-                    V<sub>IN</sub>
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      R_1_value: 240,
+      R_1_scale: 1,
+      R_2_value: 1,
+      R_2_scale: 1E3,
+      V_out: 0
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    this.computeVout();
+  }
+
+  computeVout () {
+    const Vref = 1.25,  //volts
+          Iadj = 50e-6; //50 uA
+    let R1 = this.state.R_1_value * this.state.R_1_scale;
+    let R2 = this.state.R_2_value * this.state.R_2_scale;
+    
+    this.setState({
+      V_out: Vref * (1 + R1/R2) + Iadj * R2
+    });
+  }
+
+  handleChange (evt) {
+    let z = {};
+    z[evt.currentTarget.id] = parseFloat(evt.target.value);
+    this.setState(z);
+    this.computeVout();
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1 className="text-gray-900 text-3xl title-font font-medium mb-4">LM317 simulator</h1>
+        </header>
+        <section className="text-gray-700 body-font relative">
+          <div className="container px-5 py-24 mx-auto flex sm:flex-no-wrap flex-wrap">
+            <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+              <object type="image/svg+xml" data={circuit}>
+                  <img src={circuit} />
+              </object>
+            </div>
+            <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+              {/* V_in */}
+              <div className="md:flex md:items-center mb-6">
+                  <div className="md:w-1/4">
+                    <p className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                      V<sub>IN</sub>
+                    </p>
+                  </div>
+                  <div className="md:w-2/4">
+                    <p className="bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700">
+                      Range: 4.25 ~ 40
+                    </p>
+                  </div>
+                  <div className="md:w-1/4">
+                    <span>Volts</span>
+                  </div>
+              </div>
+              {/* C_in */}
+              <div className="md:flex md:items-center mb-6">
+                <div className="md:w-1/4">
+                  <p className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                    C<sub>IN</sub>
+                  </p>
+                </div>
+                <div className="md:w-2/4">
+                  <p className="text-xs bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700">
+                    0.1 or 1.0 (ceramic or tantalum)
+                  </p>
+                </div>
+                <div className="md:w-1/4">
+                    &mu;F
+                </div>
+              </div>
+              {/* C_out */}
+              <div className="md:flex md:items-center mb-6">
+                <div className="md:w-1/4">
+                  <p className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                    C<sub>OUT</sub>
+                  </p>
+                </div>
+                <div className="md:w-2/4">
+                  <p className="text-xs bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700">
+                    1.0
+                  </p>
+                </div>
+                <div className="md:w-1/4">
+                    &mu;F
+                </div>
+              </div>
+              {/* I_adj */}
+              <div className="md:flex md:items-center mb-6">
+                <div className="md:w-1/4">
+                  <p className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                    I<sub>ADJ</sub>
+                  </p>
+                </div>
+                <div className="md:w-2/4">
+                  <p className="text-xs bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700">
+                    50 
+                  </p>
+                </div>
+                <div className="md:w-1/4">
+                    &mu;A
+                </div>
+              </div>
+              {/* R_1 */}
+              <div className="md:flex md:items-center mb-6">
+                <div className="md:w-1/4">
+                  <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="R_1">
+                    R<sub>1</sub>
                   </label>
                 </div>
-                <div class="md:w-2/4">
-                  <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                        id="V_in" type="number" min="0" max="50" step="0.1"></input>
+                <div className="md:w-2/4">
+                  <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                         type="number" step="1" min="0.0" name="R_1_value" id="R_1_value"
+                         value={this.state.R_1_value}
+                         onChange={this.handleChange}/>
                 </div>
-                <div class="md:w-1/4">
-                  <span>Volts</span>
+                <div className="md:w-1/4">
+                    <select className="md:1/3 cursor-pointer bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                            type="select" name="R_1_scale" id="R_1"
+                            value={this.state.R_2_scale} onChange={this.handleChange}>
+                      <option value="1">&Omega;</option>
+                      <option value="1E3">K&Omega;</option>
+                      <option value="1E6">M&Omega;</option>
+                    </select>
+                  </div>
+              </div>
+              {/* R_2 */}
+              <div className="md:flex md:items-center mb-6">
+                <div className="md:w-1/4">
+                  <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="R_2">
+                    R<sub>2</sub>
+                  </label>
                 </div>
-            </div>
-            {/* C_in */}
-            <div class="md:flex md:items-center mb-6">
-              <div class="md:w-1/4">
-                <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="C_in">
-                  C<sub>IN</sub>
-                </label>
-              </div>
-              <div class="md:w-2/4">
-                <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                        type="number" step="0.01" min="0.0" name="C_in_value" id="C_in_value"/>
-              </div>
-              <div class="md:w-1/4">
-                  <select class="md:1/3 bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                          type="select" name="C_in_scale" id="C_in">
-                    <option value="uF">&mu;F</option>
-                    <option value="pF">pF</option>
-                  </select>
+                <div className="md:w-2/4">
+                  <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                          type="number" step="1" min="0.0" name="R_2_value" id="R_2_value" value={this.state.R_2_value} onChange={this.handleChange}/>
                 </div>
-            </div>
-            {/* R_1 */}
-            <div class="md:flex md:items-center mb-6">
-              <div class="md:w-1/4">
-                <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="R_1">
-                  R<sub>1</sub>
-                </label>
+                <div className="md:w-1/4">
+                    <select className="md:1/3 cursor-pointer appearance-none bg-gray-200 border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                            type="select" name="R_2_scale" id="R_2"
+                            value={this.state.R_2_scale} onChange={this.handleChange}>
+                      <option value="1">&Omega;</option>
+                      <option value="1E3">K&Omega;</option>
+                      <option value="1E6">M&Omega;</option>
+                    </select>
+                  </div>
               </div>
-              <div class="md:w-2/4">
-                <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                        type="number" step="0.01" min="0.0" name="R_1_value" id="R_1_value"/>
-              </div>
-              <div class="md:w-1/4">
-                  <select class="md:1/3 bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                          type="select" name="R_1_scale" id="R_1">
-                    <option value="ohm">&Omega;</option>
-                    <option value="kom">K&Omega;</option>
-                    <option value="kom">M&Omega;</option>
-                  </select>
+              {/* V_out */}
+              <div className="md:flex md:items-center mb-6">
+                <div className="md:w-1/4">
+                  <p className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                    V<sub>out</sub>
+                  </p>
                 </div>
-            </div>
-            {/* R_2 */}
-            <div class="md:flex md:items-center mb-6">
-              <div class="md:w-1/4">
-                <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="R_2">
-                  R<sub>2</sub>
-                </label>
-              </div>
-              <div class="md:w-2/4">
-                <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                        type="number" step="0.01" min="0.0" name="R_2_value" id="R_2_value"/>
-              </div>
-              <div class="md:w-1/4">
-                  <select class="md:1/3 bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                          type="select" name="R_2_scale" id="R_2">
-                    <option value="ohm">&Omega;</option>
-                    <option value="kom">K&Omega;</option>
-                    <option value="kom">M&Omega;</option>
-                  </select>
+                <div className="md:w-2/4">
+                  <p className="text-xs bg-green-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700">
+                    {this.state.V_out.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits:2})}
+                  </p>
                 </div>
-            </div>
-            {/* C_out */}
-            <div class="md:flex md:items-center mb-6">
-              <div class="md:w-1/4">
-                <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="C_out">
-                  C<sub>OUT</sub>
-                </label>
-              </div>
-              <div class="md:w-2/4">
-                <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                        type="number" step="0.01" min="0.0" name="C_out_value" id="C_out_value"/>
-              </div>
-              <div class="md:w-1/4">
-                  <select class="md:1/3 bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                          type="select" name="C_out_scale" id="C_out">
-                    <option value="uF">&mu;F</option>
-                    <option value="pF">pF</option>
-                  </select>
+                <div className="md:w-1/4">
+                    V
                 </div>
-            </div>
-            {/* Readout */}
-            <div class="md:flex md:items-center mb-6">
-              <div class="readout bg-green-100 rounded-full md:w-1/3">
-                <div class="label">I<sub>ADJ</sub></div>
-                <div class="digit">55.1</div>
-                <div class="unit">mA</div>
-              </div>
-              <div class="readout bg-green-100 rounded-full md:w-2/3">
-              <div class="label">V<sub>out</sub></div>
-                <div class="digit">5.1</div>
-                <div class="unit">Volts</div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
-    </div>
-  );
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
